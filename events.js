d9d3cb507435294986339870de67365c763369db
todddrinkwater <todddrinkwaternz@gmail.com>
2017-01-19 21:41:42 +1300

    add event box change green color

--- events.js
+++ events.js
@@ -1,13 +1,14 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
-  one()
-    
+  one();
+  two();
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -24,6 +25,15 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two() {
+  var two = document.getElementById('two')
+
+  two.addEventListener('mouseenter', makeGreen)
+
+  two.addEventListener('mouseleave', makeWhite)
+
+}
+
 // CREATE FUNCTION three HERE
 
 // CREATE FUNCTION four HERE
@@ -36,3 +46,7 @@ function makeBlue (evt) {
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
 }
+
+function makeGreen(evt){
+  evt.target.style.backgroundColor = 'green'
+};
